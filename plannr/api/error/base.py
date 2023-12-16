bdf37aa4bcfb86c5f65348fd1a2476e6fdf78dd1
odinhufnagl <odin.hufnagl@gmail.com>
2023-12-16 19:01:02 +0100

    custom error

---
+++ plannr/api/error/base.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict, Optional
+from typing_extensions import Annotated, Doc
+from fastapi import HTTPException
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from plannr.logger import logger
+
+
+#TODO: pretty reptitive, would maybe like it to be handled more nice
+#or maybe there should be freedom, because maybe we want to do something special on a certain exception
+
+class ApiException(Exception):
+    
+    def to_response(self, req: Request):
+        return self.exception_handler(req, self)
+
+    @staticmethod
+    def exception_handler(request: Request, exc: 'ApiException'):
+        return JSONResponse(status_code=500, content={"message": "Something went wrong"})
+    
+    @classmethod
+    def add_to_app(cls, app):
+        app.add_exception_handler(cls, cls.exception_handler)
+   
+   
+class ApiWrongEmailPasswordException(ApiException):
+    
+    @staticmethod
+    def exception_handler(request: Request, exc: 'ApiException'):
+        return JSONResponse(status_code=402, content={"message": "Wrong email or password"})
+    
+    
+class ApiNoTokenException(ApiException):
+
+    @staticmethod
+    def exception_handler(request: Request, exc: 'ApiException'):
+        return JSONResponse(status_code=401, content={"message": "No token provided"})
+    
+  
+class ApiNotValidTokenException(ApiException):
+
+    @staticmethod
+    def exception_handler(request: Request, exc: 'ApiException'):
+        return JSONResponse(status_code=401, content={"message": "Not valid token"})
+    
+  
+   
--- plannr/api/middleware/auth.py
+++ plannr/api/middleware/auth.py
@@ -4,6 +4,7 @@ from requests import HTTPError
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Depends, Request, HTTPException
 import jwt
+from plannr.api.error.base import ApiNoTokenException, ApiNotValidTokenException
 
 from plannr.logger import logger
 from plannr.api.jwt_auth import jwt_user_auth
@@ -15,11 +16,11 @@ class AuthMiddleware(BaseHTTPMiddleware):
         request = AuthRequest.from_request(request)
         logger.info(request.headers)
         if not request.access_token:
-            #TODO: this will become a custom class, all errors should be custom classes
-            return JSONResponse(status_code=401, content="No Token Provided")
+            #TODO: this works but seem kinda bad...
+            return ApiNoTokenException().to_response(request)
         user_id = jwt_user_auth.decode(request.access_token)
         if not user_id:
-            return JSONResponse(status=402, content="Not a valid token")
+            return ApiNotValidTokenException().to_response(request)
 
         request.state.user_id = user_id
 
